5caf6c5afc8eff38ab12df02b9a358cda40dde62
nazo6 <53177992+nazo6@users.noreply.github.com>
2022-03-17 14:58:00 +0900

    update

--- nzskkserv-server/src/lib.rs
+++ nzskkserv-server/src/lib.rs
@@ -16,27 +16,30 @@ impl Server {
     pub async fn start(self) -> Result<()> {
         let listener = TcpListener::bind((self.address, self.port)).await?;
         loop {
-            let (mut socket, _) = listener.accept().await?;
+            let (socket, _) = listener.accept().await?;
 
             tokio::spawn(async move {
-                let mut buf = Vec::new();
-                match socket.read(&mut buf).await {
-                    Ok(0) => {
-
-                        println!("b");
-                    },
-                    Ok(n) => {
-                        println!("a");
-                        let data = &buf[..n];
-                        let str = String::from_utf8(data.to_vec()).unwrap();
-                        println!("{str}")
-                    }
-                    Err(_) => {
-
-                        println!("c");
-                    }
-                }
+                println!("Connected");
+                process(socket).await
             });
+        }
+    }
+}
+
+async fn process(mut stream: TcpStream) {
+    let mut buf = vec![0; 1];
+    match stream.read(&mut buf).await {
+        Ok(0) => {
+            println!("Connect end");
+        }
+        Ok(n) => {
+            println!("{n}");
+            let data = &buf[..n];
+            let str = String::from_utf8(data.to_vec()).unwrap();
+            println!("{str}")
+        }
+        Err(_) => {
+            println!("Error");
         }
     }
 }
