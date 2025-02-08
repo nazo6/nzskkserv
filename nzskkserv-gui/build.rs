bbbcda3bdab9a3417ec24ef7e05b979a64548691
nazo6 <git@nazo6.dev>
2025-02-08 15:32:00 +0900

    init dioxus

--- nzskkserv-gui/build.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(windows)]
-use windres::Build;
-
-fn main() {
-    #[cfg(windows)]
-    Build::new().compile("tray.rc").unwrap();
-}
--- nzskkserv-gui/src/main.rs
+++ nzskkserv-gui/src/main.rs
@@ -1,35 +1,31 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use dioxus::{
+    desktop::trayicon::{default_tray_icon, init_tray_icon},
+    prelude::*,
+};
 
-use anyhow::Result;
-use tokio::sync::watch;
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
-mod app;
-mod config;
-mod server;
-mod tray;
-
-#[derive(PartialEq, Clone, Copy, Debug)]
-enum AppState {
-    Show,
-    Hide,
-    Quit,
+fn main() {
+    dioxus::launch(App);
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    env_logger::Builder::from_env(
-        env_logger::Env::default().default_filter_or("nzskkserv_gui=info,nzskkserv_core=info"),
-    )
-    .init();
+#[component]
+fn App() -> Element {
+    init_tray_icon(default_tray_icon(), None);
 
-    let config = config::load_config().await?;
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        Hero {}
+    }
+}
 
-    let server_controller = server::start(config);
-
-    let (app_state_sender, app_state_receiver) = watch::channel(AppState::Show);
-    tray::start(app_state_sender);
-
-    app::start(app_state_receiver, server_controller);
-
-    Ok(())
+#[component]
+pub fn Hero() -> Element {
+    rsx! {
+        div { class: "px-8 py-8", "Hello" }
+    }
 }
